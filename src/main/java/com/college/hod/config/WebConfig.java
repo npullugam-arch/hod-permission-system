1135f2d0d790de9bf08054a8444d3681a4e24bd1
npullugam-arch <npullugam@gmail.com>
2026-04-05 23:58:18 +0530

    Update WebConfig.java

--- src/main/java/com/college/hod/config/WebConfig.java
+++ src/main/java/com/college/hod/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.college.hod.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,14 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadPath.toString() + "/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
